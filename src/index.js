8cd2138854613ba70fc3c0627d0dbeac95cb6aa4
Pavel <cemenru@mail.ru>
2023-01-30 22:10:58 +0300

    fixed: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let bracketsArry = bracketsConfig.map((elem) => elem.join(""));
+
+  for (let i = true; i === (bracketsArry.some((elem) => str.includes(elem)));) {
+    for (let i = 0; i < bracketsArry.length; i++) {
+      str = str.replace(bracketsArry[i], "");
+    }
+  }
+return str === "";
 }
+
+
